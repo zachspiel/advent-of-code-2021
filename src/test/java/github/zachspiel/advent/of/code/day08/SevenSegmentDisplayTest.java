fb338d4052c04382b0d05719178c3ccbf693ffc0
zachspiel <zachspielberger@gmail.com>
2021-12-15 17:41:40 -0700

    Remove generated text from day 08 test

--- src/test/java/github/zachspiel/advent/of/code/day08/SevenSegmentDisplayTest.java
+++ src/test/java/github/zachspiel/advent/of/code/day08/SevenSegmentDisplayTest.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package github.zachspiel.advent.of.code.day08;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-/**
- *
- * @author zach1
- */
 public class SevenSegmentDisplayTest {
      @Test 
      public void testPartA() {
